d70a2985b8de4437633fec093ebb09bb2289e9ff
Tempestas Ludi <tempestasludi@gmail.com>
2018-03-25 21:52:46 +0200

    Refactor router, add host to routes

--- http-source/actors/Router.cs
+++ http-source/actors/Router.cs
@@ -8,95 +8,106 @@ using com.tempestasludi.c.p23_http.data;
 
 namespace com.tempestasludi.c.p23_http.actors
 {
-	public class Router
-	{
-		private static readonly Dictionary<string, string> MimeTypes = new Dictionary<string, string>
-		{
-			{"txt", "text/plain"},
-			{"json", "application/json"},
-			{"xml", "application/xml"},
-			{"html", "text/html"},
-			{"js", "application/javascript"},
-			{"css", "text/css"},
-			{"xsl", "text/xsl"}
-		};
+  public class Router
+  {
+    private static readonly Dictionary<string, string> MimeTypes = new Dictionary<string, string>
+    {
+      {"txt", "text/plain"},
+      {"json", "application/json"},
+      {"xml", "application/xml"},
+      {"html", "text/html"},
+      {"js", "application/javascript"},
+      {"css", "text/css"},
+      {"xsl", "text/xsl"}
+    };
 
-		private readonly List<Tuple<Regex, Action<Stream, Request>, bool>> _routes =
-			new List<Tuple<Regex, Action<Stream, Request>, bool>>();
+    private readonly List<Route> _routes = new List<Route>();
 
-		public void AddRoute(Regex path, Action<Stream, Request> requestHandler, bool closeConnection = true)
-		{
-			_routes.Add(Tuple.Create(path, requestHandler, closeConnection));
-		}
+    public void AddRoute(Action<Stream, Request> requestHandler, Regex path=null, Regex host=null, bool closeConnection = true)
+    {
+      _routes.Add(new Route(path, host, requestHandler, closeConnection));
+    }
 
-		public void AddRoute(Regex path, string file)
-		{
-			AddRoute(path, (stream, request) =>
-				{
-					var match = path.Match(request.Uri);
-					var subPath = request.Uri.Substring(match.Length);
-					var location = Path.Combine(file, subPath);
-					if (File.Exists(location))
-					{
-						SendFile(stream, location);
-					}
-					else if (File.Exists(Path.Combine(location, "index.xml")))
-					{
-						SendFile(stream, Path.Combine(location, "index.xml"));
-					}
-					else if (File.Exists(Path.Combine(location, "index.html")))
-					{
-						SendFile(stream, Path.Combine(location, "index.html"));
-					}
-					else
-					{
-						new Response
-						{
-							Content = Encoding.UTF8.GetBytes("<html><body><h1>Not Found</h1></body></html>"),
-							ContentType = "text/html",
-							Status = "Not Found",
-							StatusCode = 404
-						}.Write(stream);
-					}
-				}
-			);
-		}
+    public void AddRoute(string file, Regex path, Regex host = null)
+    {
+      AddRoute((stream, request) =>
+      {
+        var match = path.Match(request.Uri);
+        var subPath = request.Uri.Substring(match.Length);
+        var location = Path.Combine(file, subPath);
+        try
+        {
+          SendFile(stream, new[]
+            {
+              location,
+              Path.Combine(location, "index.xml"),
+              Path.Combine(location, "index.html")
+            }.SkipWhile(f => !File.Exists(f))
+            .First());
+        }
+        catch (InvalidOperationException)
+        {
+          new Response
+          {
+            Content = Encoding.UTF8.GetBytes("<html><body><h1>Not Found</h1></body></html>"),
+            ContentType = "text/html",
+            Status = "Not Found",
+            StatusCode = 404
+          }.Write(stream);
+        }
+      }, path, host);
+    }
 
-		private static void SendFile(Stream stream, string path)
-		{
-			var extension = Path.GetExtension(path).Substring(1);
-			new Response
-			{
-				Content = File.ReadAllBytes(path),
-				ContentType = MimeTypes.ContainsKey(extension) ? MimeTypes[extension] : null,
-				Status = "OK",
-				StatusCode = 200
-			}.Write(stream);
-		}
+    private static void SendFile(Stream stream, string path)
+    {
+      var extension = Path.GetExtension(path)?.Substring(1) ?? "";
+      new Response
+      {
+        Content = File.ReadAllBytes(path),
+        ContentType = MimeTypes.ContainsKey(extension) ? MimeTypes[extension] : null,
+        Status = "OK",
+        StatusCode = 200
+      }.Write(stream);
+    }
 
-		public bool Route(Stream stream, Request request)
-		{
-			var usedRoute = _routes.Where(route =>
-			{
-				var match = route.Item1.Match(request.Uri);
-				return match.Success && request.Uri.Substring(0, match.Length) == match.Value;
-			}).FirstOrDefault();
+    public bool Route(Stream stream, Request request)
+    {
+      var uri = request.Uri;
+      var host = request.Host ?? "";
+      var usedRoute = _routes.SkipWhile(route => route.PathRegex.IsMatch(uri) && route.HostRegex.IsMatch(host))
+        .FirstOrDefault();
 
-			if (usedRoute != null)
-			{
-				usedRoute.Item2(stream, request);
-				return usedRoute.Item3;
-			}
+      if (usedRoute != null)
+      {
+        usedRoute?.RequestHandler(stream, request);
+        return usedRoute.CloseConnection;
+      }
 
-			new Response
-			{
-				Content = Encoding.UTF8.GetBytes("<html><body><h1>Not Found</h1></body></html>"),
-				ContentType = "text/html",
-				Status = "Not Found",
-				StatusCode = 404
-			}.Write(stream);
+      new Response
+      {
+        Content = Encoding.UTF8.GetBytes("<html><body><h1>Not Found</h1></body></html>"),
+        ContentType = "text/html",
+        Status = "Not Found",
+        StatusCode = 404
+      }.Write(stream);
 
-			return true;
-		}
-	}
+      return true;
+    }
+  }
+
+  internal class Route
+  {
+    public readonly Regex PathRegex;
+    public readonly Regex HostRegex;
+    public readonly Action<Stream, Request> RequestHandler;
+    public readonly bool CloseConnection;
+
+    public Route(Regex pathRegex, Regex hostRegex, Action<Stream, Request> requestHandler, bool closeConnection)
+    {
+      PathRegex = pathRegex ?? new Regex(".*");
+      HostRegex = hostRegex ?? new Regex(".*");
+      RequestHandler = requestHandler;
+      CloseConnection = closeConnection;
+    }
+  }
 }
