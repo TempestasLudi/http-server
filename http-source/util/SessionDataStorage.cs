d2b90848231a67c59274d06188aa10f88dc25541
Tempestas Ludi <tempestasludi@gmail.com>
2018-03-25 12:51:00 +0200

    Add a session class, expiration and comments to the session data storage

--- http-source/util/SessionDataStorage.cs
+++ http-source/util/SessionDataStorage.cs
@@ -1,36 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace com.tempestasludi.c.p23_http.util
 {
+  
+  /// <summary>
+  /// Stores data that is related to a session, each session linked to a guid.
+  /// </summary>
   public class SessionDataStorage
   {
-    private Dictionary<Guid, Dictionary<string, object>> sessions = new Dictionary<Guid, Dictionary<string, object>>();
+    /// <summary>
+    /// The dictionary for finding sessions based on their id.
+    /// </summary>
+    private Dictionary<Guid, Session> _sessions = new Dictionary<Guid, Session>();
 
-    private (Guid, Dictionary<string, object>) CreateSession()
+    /// <summary>
+    /// Creates a new session.
+    /// </summary>
+    /// <returns>The session.</returns>
+    private Session CreateSession()
     {
-      var id = Guid.NewGuid();
-      if (sessions.ContainsKey(id))
-      {
-        throw new Exception("Cannot create session. Duplicate Guid.");
-      }
-      sessions[id] = new Dictionary<string, object>();
-      return (id, sessions[id]);
+      var session = new Session();
+      _sessions[session.Id] = session;
+      return session;
     }
 
-    // TODO: Error handling
-    private Dictionary<string, object> GetSession(Guid id)
+    /// <summary>
+    /// Retrieves a session based on an id.
+    /// </summary>
+    /// <param name="id">The id of the session to retrieve.</param>
+    /// <returns>The session with the specified id.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown when no session exists with the specified id.</exception>
+    private Session GetSession(Guid id)
     {
-      if (sessions.ContainsKey(id))
+      if (_sessions.ContainsKey(id))
       {
-        return sessions[id];
+        return _sessions[id];
       }
-      throw new Exception("No session with given id exists");
+      throw new KeyNotFoundException("No session with given id exists");
     }
 
-    private Dictionary<string, object> GetSession(string id)
+    /// <summary>
+    /// Retrieves a session based on an id string.
+    /// </summary>
+    /// <param name="id">The id string of the session to retrieve.</param>
+    /// <returns>The session with the specified id.</returns>
+    private Session GetSession(string id)
     {
       return GetSession(Guid.Parse(id));
+    }
+
+    /// <summary>
+    /// Deletes the expired sessions.
+    /// </summary>
+    public void Cleanup()
+    {
+      _sessions = _sessions.Where(entry => entry.Value.Expiration <= DateTime.Now).ToDictionary(s => s.Key, s => s.Value);
+    }
+  }
+
+  /// <summary>
+  /// Maintains the data of a session.
+  /// </summary>
+  public class Session
+  {
+    /// <summary>
+    /// The id of the session.
+    /// </summary>
+    public readonly Guid Id = Guid.NewGuid();
+    
+    /// <summary>
+    /// The date when the session expires.
+    /// </summary>
+    public DateTime Expiration = DateTime.Now.AddHours(6);
+    
+    /// <summary>
+    /// The objects, associated with a string name, that constitute the data of the session.
+    /// </summary>
+    public readonly Dictionary<string, object> Data = new Dictionary<string, object>();
+
+    /// <summary>
+    /// Postpones the expiration of the session to a number of hours from now.
+    /// </summary>
+    /// <param name="hourCount">The number of hours to postpone the expiration with.</param>
+    public void PostponeExpiration(int hourCount = 6)
+    {
+      Expiration = DateTime.Now.AddHours(hourCount);
     }
   }
 }
